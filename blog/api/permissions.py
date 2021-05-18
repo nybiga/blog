a00ee2d64b54c94ab8157b0c3cecb410534848cb
nybiga <63249383+nybiga@users.noreply.github.com>
2021-05-18 10:16:18 +0000

    first commit

---
+++ blog/api/permissions.py
@@ -0,0 +1,26 @@
+from rest_framework import permissions
+from rest_framework import permissions
+from api.permissions import IsOwnerOrReadOnly
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.owner == request.user
+
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
